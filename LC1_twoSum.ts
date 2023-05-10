f5bb09c9224774de3f00796214b900329b8f10a0
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-10 11:08:37 -0400

    modify and add comment

--- LC1_twoSum.ts
+++ LC1_twoSum.ts
@@ -1,16 +1,27 @@
-// Take some function (TwoSums) that takes in some number array and a target (non-array) set the property as a number array. 
-
-// Initialize i as 0 then iterate for i < the array's length, and iterative through i as i increases. 
-// Initialize second array (j) as i + 1 (we want it greater than i), then iterate that for i < array length of (i) then increment j. 
-// Check value of num array at index i and add to num array at j check if the values equal to target, if it is then return i and j otherwise return an empty array. 
+/**
+ * @author Mark
+ * @param num 
+ * @param target 
+ * @returns 
+ */
 
 function twoSum(num: number[], target: number): number[] {
-    for(let i = 0; i < num.length; i++) {
-        for (let j = i + 1; j < num.length; j++) {
-            if(num[i] + num[j] === target) {
-                return [i,j]
-            }
+    // This function finds two numbers in the array `num` that add up to the target `target`.
+    // The function returns an array of two numbers, where the first number is the index of the first number in the array and the second number is the index of the second number in the array.
+    // If there are no two numbers in the array that add up to the target, the function returns an empty array.
+  
+    // Iterate over the array `num`.
+    for (let i = 0; i < num.length; i++) {
+      // Iterate over the array `num` starting at the index `i + 1`.
+      for (let j = i + 1; j < num.length; j++) {
+        // If the sum of the two numbers at the `i` and `j` indices is equal to the target...
+        if (num[i] + num[j] === target) {
+          // Return an array with the `i` and `j` indices.
+          return [i, j];
         }
+      }
     }
-    return []
-}
+  
+    // If the function reaches this point, then there are no two numbers in the array that add up to the target.
+    return [];
+  }
