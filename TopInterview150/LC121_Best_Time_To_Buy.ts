0fb472b4b2b1ba5fb06680d17b07734f1f46fe8f
Mr. Cain <58246556+DrDrewCain@users.noreply.github.com>
2023-05-10 11:06:25 -0400

    modify LC121

--- TopInterview150/LC121_Best_Time_To_Buy.ts
+++ TopInterview150/LC121_Best_Time_To_Buy.ts
@@ -1,35 +1,49 @@
-    /**
-     * Finds the maximum profit that can be achieved by buying and selling a stock.
-     * @author Mark
-     * @param prices An array of prices.
-     * @param fee The transaction fee.
-     * @returns The maximum profit.
-     * Args:
-     *   prices: An array of prices.
-     *
-     * Returns:
-     *   The maximum profit.
-     */
-  
-function maxProfit(prices: number[]): number {
+/**
+ * Finds the maximum profit that can be achieved by buying and selling a stock.
+ * @author Mark
+ * @param prices
+ * Args:
+ *   prices: An array of prices.
+ *
+ * Returns:
+ *   The maximum profit.
+ */
 
-    // Initialize the min price and max profit.
-    let minPrice = Infinity; // Initialize the min price to infinity. This is a very large number, so any price will be less than it.
-    let maxProfit = 0; // Initialize the max profit to 0.
-  
-    // Iterate over the prices.
-    for (const price of prices) {
-      // Update the min price to the current price if it is less than the current min price.
-      minPrice = Math.min(minPrice, price);
-  
-      // Update the max profit to the current profit if it is greater than the current max profit.
-      maxProfit = Math.max(maxProfit, price - minPrice);
-    }
-  
-    // Return the max profit.
-    return maxProfit;
+function maxProfit(prices: number[]): number {
+  /**
+   * Initialize the min price and max profit.
+   *
+   * minPrice: The minimum price of the stock.
+   * maxProfit: The maximum profit that can be achieved by buying and selling the stock.
+   */
+  let minPrice = Infinity; // Initialize the min price to infinity. This is a very large number, so any price will be less than it.
+  let maxProfit = 0; // Initialize the max profit to 0.
+
+  /**
+   * Iterate over the prices.
+   *
+   * price: The current price of the stock.
+   */
+  for (const price of prices) {
+    /**
+     * Update the min price to the current price if it is less than the current min price.
+     */
+    minPrice = Math.min(minPrice, price);
+
+    /**
+     * Update the max profit to the current profit if it is greater than the current max profit.
+     *
+     * profit: The profit that can be achieved by buying the stock at the min price and selling it at the current price.
+     */
+    let profit = price - minPrice;
+    maxProfit = Math.max(maxProfit, profit);
   }
 
+  /**
+   * Return the max profit.
+   */
+  return maxProfit;
+}
 
 /**
  * Alternative approach
